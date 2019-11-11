64211386269276bcbccdddc074866ab95419bfb9
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-11 22:39:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+  if block_given?
+    index = 0
+    while index < array.length do 
+      yield(array[index])
+      i += 1
+    end
+  else
+    puts "It's empty"
+  end
+end
 
