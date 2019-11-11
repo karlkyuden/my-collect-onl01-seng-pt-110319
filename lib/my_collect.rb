11c6000eb686aeb0d8d0355ec04de31c017f947f
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-11 22:41:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,6 +10,11 @@ def my_collect(array)
   end
 end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
   lang.upcase
